342543fa5cfd0c5e8759c6dc24b662385924918c
Zachary Ives <zives@hawk.iit.edu>
2024-01-19 16:32:22 -0600

    Initial Commit

---
+++ Spherical Bessel and Neumann Function Calculations/Spherical_Bessel_and_Neumann_Function_CalculationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spherical_Bessel_and_Neumann_Function_CalculationsApp.swift
+//  Spherical Bessel and Neumann Function Calculations
+//
+//  Created by Phys440Zachary on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Spherical_Bessel_and_Neumann_Function_CalculationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
